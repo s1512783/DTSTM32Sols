186ab7c250aeb943d7306c6fd791faa233ca4901
s1512783 <marcin.morawski@zoho.com>
2018-04-06 22:43:04 +0100

    A faster version of ex. 4.2 which polls for time to catch brief button pushes

--- 4.2 - Blinky + Buttons/main.c
+++ 4.2 - Blinky + Buttons/main.c
@@ -2,9 +2,15 @@
 # include <stm32f10x_gpio.h>
 # include <stm32f10x.h>
 
-void Delay(uint32_t nTime);
+/* Blinky + Buttons Blinking a led with a pushbutton. The button is on port A.
+ * It uses polling to get the button value. The proper way to do this would be
+ * with an interrupt, but I don't know how to do that yet.
+ *
+ * Marcin Morawski, 06.04.2018
+ */
 
-// Blinky + Buttons Blinking a led with a pushbutton. The button is on port A.
+
+int checkDelay(uint32_t nTime);
 
 int main(void)
 {
@@ -14,14 +20,11 @@ int main(void)
 	// Configure Pins
 	GPIO_StructInit(&GPIO_InitStructure);
 	// LEDS
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_8;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
+
 	// Pushbutton
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
@@ -32,32 +35,41 @@ int main(void)
 	if (SysTick_Config(SystemCoreClock/1000))
 		while (1);
 
+
 	while (1) {
 		static int ledval = 0;
-		// toggle led
+		// toggle led if delay time faster
+		if (checkDelay(250)) {
 		GPIO_WriteBit(GPIOC, GPIO_Pin_9, (ledval) ? Bit_SET : Bit_RESET);
 		ledval = 1 - ledval;
+		}
+
 		if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0))
 			GPIO_WriteBit(GPIOC, GPIO_Pin_8, Bit_SET);
 		else
 			GPIO_WriteBit(GPIOC, GPIO_Pin_8, Bit_RESET);
-		Delay (250); // wait 250 ms
 	}
 }
 
-// Timer code
-static __IO uint32_t TimingDelay;
+/* We're using timeKeeper to check if the delay time of the green LED has elapsed
+ * The SysTick_Handler just has to increment it
+*/
+static __IO uint32_t timeKeeper = 0;
 
-void Delay(uint32_t nTime)
+int checkDelay(uint32_t nTime)
 {
-	TimingDelay = nTime;
-	while (TimingDelay != 0);
+	if (timeKeeper >= nTime){
+		timeKeeper = 0;
+		return 1;
+	}
+	else
+		return 0;
 }
+
 
 void SysTick_Handler(void)
 {
-	if (TimingDelay != 0x00)
-		TimingDelay --;
+	timeKeeper++;
 }
 
 
