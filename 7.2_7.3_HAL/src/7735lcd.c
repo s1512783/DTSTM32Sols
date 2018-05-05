7206fa8b25d91e18a99ac1a51c2a0525f4db8701
M <m@localhost.localdomain>
2018-05-05 22:30:33 +0100

    Proceeding with solutions to ex. 7.2 & 7.3, this is commit before trying to import new cubeMX config into an existing project

---
+++ 7.2_7.3_HAL/src/7735lcd.c
@@ -0,0 +1,335 @@
+#include "stm32f1xx_hal.h"
+#include "7735lcd.h"
+#include "font.h"
+
+// pin definitions
+#define LCD_PORT GPIOC
+#define LCD_PORT_BKL GPIOA
+#define GPIO_PIN_DC GPIO_PIN_2 // on port GPIOC
+#define GPIO_PIN_RST GPIO_PIN_1 // on port GPIOC
+#define GPIO_PIN_SCE GPIO_PIN_0 // on port GPIOC
+#define GPIO_PIN_BKL GPIO_PIN_1 // on port GPIOA
+#define SPILCD &hspi2
+
+// LCD control
+#define LOW 0
+#define HIGH 1
+#define LCD_C LOW
+#define LCD_D HIGH
+#define ST7735_CASET 0x2A
+#define ST7735_RASET 0x2B
+#define ST7735_MADCTL 0x36
+#define ST7735_RAMWR 0x2C
+#define ST7735_RAMRD 0x2E
+#define ST7735_COLMOD 0x3A
+
+// the book or's MADVAL with 0x08, which inverts the color byte order in my display
+//(I think it depends on the version of 7735 you have)
+
+#define MADVAL(x) (((x) << 5))
+
+struct ST7735_cmdBuf
+{
+	uint8_t command; //ST7735 command byte
+	uint8_t delay; // ms delay after
+	uint8_t len; // length of parameter data
+	uint8_t data [16]; //parameter data
+};
+
+
+static const struct ST7735_cmdBuf initializers[] = {
+		// SWRESET Software reset
+		{0x01, 150, 0, 0},
+		// SLPOUT Leave sleep mode
+		{0x11, 150, 0, 0},
+		// FRMCTR1 , FRMCTR2 Frame Rate configuration -- Normal mode , idle
+		// frame rate = fosc / (1 x 2 + 40) * (LINE + 2C + 2D)
+		{0xB1 , 0, 3, {0x01 , 0x2C, 0x2D}},
+		{0xB2 , 0, 3, {0x01 , 0x2C, 0x2D}},
+		// FRMCTR3 Frame Rate configureation -- partial mode
+		{0xB3 , 0, 6, {0x01, 0x2C, 0x2D, 0x01, 0x2C, 0x2D}},
+		// INVCTR Display inversion (no inversion)
+		{0xB4, 0 , 1, {0x07}},
+		// PWCTR1 Power control -4.6V, Auto mode
+		{0xC0, 0, 3, {0xA2, 0x02, 0x84}},
+		// PWCTR2 Power control VGH25 2.4C, VGSEL -10, VGH = 3 * AVDD
+		{0xC1, 0, 1, {0xC5}},
+		// PWCTR3 Power control, opamp current small, boost frequency
+		{0xC2, 0, 2, {0x0A, 0x00}},
+		// PWCTR4 Power control , BLK/2, opamp current small and medium low
+		{0xC3, 0, 2, {0x8A , 0x2A}},
+		// PWRCTR5, VMCTR1 Power control
+		{0xC4, 0, 2, {0x8A, 0xEE}},
+		{0xC5, 0, 1, {0x0E}},
+		// INVOFF Don't invert display
+		{0x20, 0, 0, 0},
+		// Memory access directions . row address /col address , bottom to top refesh (10.1.27)
+		{ST7735_MADCTL, 0, 1, {MADVAL(MADCTLGRAPHICS)}},
+
+		/* CHECK DISPLAY MODEL (REG/GREEN/BLACK)*/
+
+		// Color mode 16 bit (10.1.30)
+		{ST7735_COLMOD, 10, 1, {0x05}},
+		// Column address set 0..127
+		{ST7735_CASET, 0, 4, {0x00, 0x02, 0x00, 0x7F}},
+		// Row address set 0..159
+		{ST7735_RASET, 0, 4, {0x00, 0x01, 0x00, 0x9F}},
+		// GMCTRP1 Gamma correction
+		{0xE0, 0, 16, {0x02, 0x1C, 0x07, 0x12, 0x37, 0x32, 0x29, 0x2D,
+				0x29, 0x25, 0x2B, 0x39, 0x00, 0x01, 0x03, 0x10}},
+				// GMCTRP2 Gamma Polarity correction
+				{0xE1, 0, 16, {0x03, 0x1d, 0x07, 0x06, 0x2E, 0x2C, 0x29, 0x2D,
+						0x2E, 0x2E, 0x37, 0x3F, 0x00, 0x00, 0x02, 0x10}},
+
+						/* ADAFRUIT HAS DISPON AND NORON SWAPPED */
+
+						// DISPON Display on
+						{0x29, 100, 0, 0},
+						// NORON Normal on
+						{0x13, 10, 0, 0},
+						// End
+						{0, 0, 0, 0}
+};
+
+
+// low level routines
+/* LcdWrite - send 8-bit data to LCD over SPI*/
+static void LcdWrite(char dc, const char *data, int cnt)
+{
+	HAL_GPIO_WritePin(LCD_PORT, GPIO_PIN_DC, dc); // we use the DC pin to distinguish between data and control sequences
+	HAL_GPIO_WritePin(LCD_PORT, GPIO_PIN_SCE, RESET); // select LCD on SPI by pulling its pin low
+	HAL_SPI_TransmitReceive(SPILCD, data, 0, cnt, HAL_MAX_DELAY); // send SPI data
+	HAL_GPIO_WritePin(LCD_PORT, GPIO_PIN_SCE, SET); // disassert LCD on SPI
+}
+
+/* LcdWrite - send 16-bit data to LCD over SPI*/
+static void LcdWrite16(char dc, const uint16_t *data, int cnt)
+{
+	HAL_GPIO_WritePin(LCD_PORT, GPIO_PIN_DC, dc);
+	HAL_GPIO_WritePin(LCD_PORT, GPIO_PIN_SCE, RESET);
+	// change spi data size to 16 bit
+	(hspi2->Instance->CR1) |= (~(uint16_t)SPI_DATASIZE_16BIT);
+	spiReadWrite16(SPILCD, 0, data, cnt, LCDSPEED);
+	// change spi data size back to 8 bit
+	(hspi2->Instance->CR1) &= (~(uint16_t)SPI_DATASIZE_16BIT); // clear DFF value
+	(hspi2->Instance->CR1) |= (~(uint16_t)SPI_DATASIZE_8BIT); // set it back to 8bit, I could probably get away with just zeroing it all
+	HAL_GPIO_WritePin(LCD_PORT, GPIO_PIN_SCE, SET);
+}
+
+/* ST7735_writeCmd - send a command to the ST7735 chip*/
+static void ST7735_writeCmd(uint8_t c)
+{
+	LcdWrite(LCD_C, &c, 1);
+}
+
+
+/*  ST7735_init - initialise the LCD and SPI stuff */
+void ST7735_init()
+{
+	const struct ST7735_cmdBuf *cmd;
+
+	/*Initialise SPI*/
+	/* set cs , reset low  - I've no idea why this needs to happen. Maybe we're resetting the configuration of the LCD chip?*/
+	HAL_GPIO_WritePin(LCD_PORT, GPIO_PIN_SCE, HIGH);
+	HAL_GPIO_WritePin(LCD_PORT, GPIO_PIN_RST, HIGH);
+	HAL_GPIO_WritePin(LCD_PORT, GPIO_PIN_RST, LOW);
+	HAL_Delay(10);
+	HAL_GPIO_WritePin(LCD_PORT, GPIO_PIN_RST, HIGH);
+	HAL_Delay(10);
+
+	// Send initialization commands to ST7735
+	for (cmd = initializers; cmd->command; cmd ++){
+		LcdWrite(LCD_C, &(cmd->command), 1); // why are they dereferencing the pointer and then referencing it again?
+		if (cmd->len)
+			LcdWrite(LCD_D, cmd->data , cmd->len);
+		if (cmd->delay)
+			HAL_Delay(cmd->delay);
+	}
+}
+
+
+static uint8_t madctlcurrent = MADVAL(MADCTLGRAPHICS); // for storing current control sequence
+
+/* ST7735_setAddrWindow - send window in which we'll be drawing pixels */
+void ST7735_setAddrWindow(uint16_t x0, uint16_t y0,
+		uint16_t x1, uint16_t y1, uint8_t madctl)
+{
+	madctl = MADVAL(madctl);
+	if (madctl != madctlcurrent){
+		ST7735_writeCmd(ST7735_MADCTL);
+		LcdWrite(LCD_D, &madctl, 1);
+		madctlcurrent = madctl;
+	}
+
+	// set column boundaries
+	ST7735_writeCmd(ST7735_CASET);
+	LcdWrite16(LCD_D, &x0, 1);
+	LcdWrite16(LCD_D, &x1, 1);
+
+	// set row boundaries
+	ST7735_writeCmd(ST7735_RASET);
+	LcdWrite16(LCD_D, &y0, 1);
+	LcdWrite16(LCD_D, &y1, 1);
+
+	// send write command to start writing
+	ST7735_writeCmd(ST7735_RAMWR);
+}
+
+/* ST7735_pushColor - write pixel color data to LCD RAM */
+void ST7735_pushColor(uint16_t *color, int cnt)
+{
+	LcdWrite16(LCD_D, color, cnt);
+}
+
+void ST7735_fillRect(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color)
+{
+	uint8_t x,y;
+	ST7735_setAddrWindow(x0, y0, x1 - 1, y1 - 1, MADCTLGRAPHICS);
+	for (x=x0; x < x1; x++) {
+		for (y=y0; y < y1; y++) {
+			ST7735_pushColor(&color, 1);
+		}
+	}
+}
+
+
+/* ST7735_drawChar - draws character C from ASCII bmp font defined in font.h */
+void ST7735_drawChar(uint16_t x0, uint16_t y0,
+		unsigned char c, uint16_t textColor, uint16_t bgColor)
+{
+	uint8_t i, j;
+
+	// For some reason the Adafruit fonts seem to be rotated 90 degrees. I fixed this by swapping all the y's and x's.
+	// This required defining a new memory access type. Not elegant, but works.
+	ST7735_setAddrWindow(y0, x0, y0 + 9, x0 + 6, MADCTLTEXT);
+	// each character is stored as 5 lines
+	for (i = 0; i < 5; i++){
+
+		uint8_t line = font[5*c + i]; // get line from font
+
+		//draw letter and single line spacing
+		for (j=0; j < 8; j++, line >>= 1){
+			if (line & 0x01)
+				ST7735_pushColor(&textColor, 1);
+			else
+				ST7735_pushColor(&bgColor, 1);
+		}
+
+		// draw line spacing
+		ST7735_pushColor(&bgColor, 2);
+	}
+
+	// draw character spacing
+	// ST7735_pushColor(&bgColor, 10); -- can't do this
+	// For some reason it gives erratic errors in the vertical line (random pixels turned on)
+	for (j = 0; j<10; j++)
+		ST7735_pushColor(&bgColor, 1);
+}
+
+
+/* ST7735_printStringRect - print as much of str as will fit in rectangle defined by x0, y0, x1, y1.
+ * Return number of chars printed
+ */
+uint8_t ST7735_printStringRect(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
+		unsigned char *str, uint16_t textColor, uint16_t bgColor)
+{
+	uint8_t i, j, lineLen, noLines, noWritten, endString;
+
+	// calculate drawing space
+	lineLen = (x1 - x0) / CHARWIDTH;
+	noLines = (y1 - y0) / CHARHEIGHT;
+	endString = 0;
+
+	for (i = 0; i < noLines; i++){
+		if (!endString){
+			for (j = 0; j < lineLen; j++) {
+				if (*str == '\n'){ // skip line if newline
+					str++;
+					break;
+				}
+				if (*str == '\0') { // stop printing chars if end of string
+					endString = 1;
+					break;
+				}
+				ST7735_drawChar(x0 + j*CHARWIDTH, y0 + i*CHARHEIGHT, *str++, textColor, bgColor);
+				noWritten++;
+			}
+		}
+		else
+			break;
+	}
+
+
+	return noWritten;
+}
+
+void ST7735_drawVertLine(uint16_t x0, uint16_t y0,
+		uint8_t length, uint8_t thickness,  uint16_t lineColor)
+{
+	ST7735_fillRect(x0, y0, x0 + thickness, y0 + length, lineColor);
+}
+
+void ST7735_drawHorizLine(uint16_t x0, uint16_t y0,
+		uint8_t length, uint8_t thickness,  uint16_t lineColor)
+{
+	ST7735_fillRect(x0, y0, x0 + length, y0 + thickness, lineColor);
+}
+
+void ST7735_drawRect(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1,
+		uint8_t thickness,  uint16_t lineColor)
+{
+	ST7735_drawVertLine(x0, y0, y1 - y0, thickness, lineColor);
+	ST7735_drawHorizLine(x0 + thickness, y1 - thickness, x1 - x0 - thickness, thickness, lineColor);
+	ST7735_drawHorizLine(x0 + thickness, y0, x1 - x0 - thickness, thickness, lineColor);
+	ST7735_drawVertLine(x1 - thickness, y0 + thickness, y1 - y0 - 2 * thickness, thickness, lineColor);
+}
+
+void ST7735_drawPixel(uint16_t x0, uint16_t y0, uint16_t color)
+{
+	ST7735_setAddrWindow(x0, y0, x0+1, y0+1, MADCTLGRAPHICS);
+	ST7735_pushColor(&color, 1);
+}
+
+void ST7735_drawCircle(uint16_t x0, uint16_t y0, uint16_t r, uint16_t color)
+{
+	int16_t f = 1 - r;
+	int16_t ddF_x = 1;
+	int16_t ddF_y = -2 * r;
+	int16_t x = 0;
+	int16_t y = r;
+
+	ST7735_drawPixel(x0  , y0+r, color);
+	ST7735_drawPixel(x0  , y0-r, color);
+	ST7735_drawPixel(x0+r, y0  , color);
+	ST7735_drawPixel(x0-r, y0  , color);
+
+	while (x<y) {
+		if (f >= 0) {
+			y--;
+			ddF_y += 2;
+			f += ddF_y;
+		}
+		x++;
+		ddF_x += 2;
+		f += ddF_x;
+
+		ST7735_drawPixel(x0 + x, y0 + y, color);
+		ST7735_drawPixel(x0 - x, y0 + y, color);
+		ST7735_drawPixel(x0 + x, y0 - y, color);
+		ST7735_drawPixel(x0 - x, y0 - y, color);
+		ST7735_drawPixel(x0 + y, y0 + x, color);
+		ST7735_drawPixel(x0 - y, y0 + x, color);
+		ST7735_drawPixel(x0 + y, y0 - x, color);
+		ST7735_drawPixel(x0 - y, y0 - x, color);
+	}
+}
+
+
+/* ST7735_backLight - turn LCD backlight on and off */
+void ST7735_backLight(uint8_t on)
+{
+	if(on)
+		HAL_GPIO_WritePin(LCD_PORT_BKL, GPIO_PIN_BKL, HIGH);
+	else
+		HAL_GPIO_WritePin(LCD_PORT_BKL, GPIO_PIN_BKL, LOW);
+}
--- 7.2_7.3_HAL/src/main.c
+++ 7.2_7.3_HAL/src/main.c
@@ -1,44 +1,46 @@
 
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  ** This notice applies to any and all portions of this file
-  * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
-  * inserted by the user or by software development tools
-  * are owned by their respective copyright owners.
-  *
-  * COPYRIGHT(c) 2018 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ ** This notice applies to any and all portions of this file
+ * that are not between comment pairs USER CODE BEGIN and
+ * USER CODE END. Other portions of this file, whether
+ * inserted by the user or by software development tools
+ * are owned by their respective copyright owners.
+ *
+ * COPYRIGHT(c) 2018 STMicroelectronics
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "stm32f1xx_hal.h"
+#include "7735lcd.h"
+
 
 /* USER CODE BEGIN Includes */
 
@@ -67,150 +69,211 @@ static void MX_SPI2_Init(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  *
-  * @retval None
-  */
+ * @brief  The application entry point.
+ *
+ * @retval None
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration----------------------------------------------------------*/
+	/* MCU Configuration----------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
+	ST7735_init();
+	ST7735_backLight(1);
+	/* USER CODE END Init */
 
-  /* USER CODE END Init */
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE END SysInit */
 
-  /* USER CODE END SysInit */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_SPI2_Init();
+	/* USER CODE BEGIN 2 */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_SPI2_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
+	// Test screen borders
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+	ST7735_fillRect(0, 0, ST7735_width, ST7735_height, ST7735_BLUE); // blue backgruond to see the text bgnd
 
-  /* USER CODE END WHILE */
+	ST7735_drawChar(0, 0, 'A', ST7735_WHITE, ST7735_BLACK);
+	ST7735_drawChar(ST7735_width-CHARWIDTH, 0, 'B', ST7735_WHITE, ST7735_BLACK);
+	ST7735_drawChar(ST7735_width-CHARWIDTH, ST7735_height-CHARHEIGHT, 'C', ST7735_WHITE, ST7735_BLACK);
+	ST7735_drawChar(0, ST7735_height-CHARHEIGHT, 'D', ST7735_WHITE, ST7735_BLACK);
 
-  /* USER CODE BEGIN 3 */
+	// Test single char print
+	unsigned char str1[] = "Thank you Adafruit";
 
-  }
-  /* USER CODE END 3 */
+	unsigned char *ptr = str1;
+
+	while (*ptr != '\0'){
+		ST7735_drawChar(ST7735_width/2, ST7735_height/2, *ptr++, ST7735_WHITE, ST7735_BLACK);
+		HAL_Delay(300);
+	}
+
+	// Test line print
+
+	ST7735_fillRect(0, 0, ST7735_width, ST7735_height, ST7735_BLUE);
+
+	unsigned char str2[] = "The quick brown fox\n"
+			"jumps over the lazy\ndog";
+
+	ST7735_printStringRect(0, 0, ST7735_width - 1, ST7735_height - 1, str2, ST7735_WHITE, ST7735_BLACK);
+
+	HAL_Delay(500);
+
+	// Test shape prints
+
+
+	ST7735_fillRect(0, 0, ST7735_width, ST7735_height, ST7735_BLACK);
+
+	ST7735_drawVertLine(ST7735_width/2, 0, ST7735_height, 2, ST7735_WHITE);
+
+	ST7735_drawHorizLine(0, ST7735_height/2, ST7735_width, 2, ST7735_WHITE);
+
+	HAL_Delay(500);
+
+	ST7735_fillRect(0, 0, ST7735_width, ST7735_height, ST7735_BLACK);
+
+	ST7735_drawRect(10, ST7735_height - 50, ST7735_width - 10, ST7735_height-10, 2, ST7735_WHITE);
+
+	unsigned char str3[] = "The quick brown fox "
+			"jumps over\nthe lazy dog";
+
+	ST7735_printStringRect(14, ST7735_height - 46, ST7735_width - 14, ST7735_height-14, str3, ST7735_WHITE, ST7735_BLACK);
+
+	uint16_t radius = 30;
+
+	/* USER CODE END 2 */
+
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1)
+	{
+		if(!radius){
+			radius = 30;
+			ST7735_fillRect(ST7735_width/2 - 30, 25, ST7735_width/2 + 30, 85, ST7735_BLACK);
+		}
+
+		ST7735_drawCircle(ST7735_width/2, 55, radius = radius - 5, ST7735_WHITE);
+		HAL_Delay(100);
+
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
+
+	}
+	/* USER CODE END 3 */
 
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
 
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 16;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+	/**Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = 16;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		_Error_Handler(__FILE__, __LINE__);
+	}
 
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/**Initializes the CPU, AHB and APB busses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+	{
+		_Error_Handler(__FILE__, __LINE__);
+	}
 
-    /**Configure the Systick interrupt time 
-    */
-  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+	/**Configure the Systick interrupt time
+	 */
+	HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
 
-    /**Configure the Systick 
-    */
-  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+	/**Configure the Systick
+	 */
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-  /* SysTick_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+	/* SysTick_IRQn interrupt configuration */
+	HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
 /* SPI2 init function */
 static void MX_SPI2_Init(void)
 {
 
-  /* SPI2 parameter configuration*/
-  hspi2.Instance = SPI2;
-  hspi2.Init.Mode = SPI_MODE_MASTER;
-  hspi2.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
-  hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi2.Init.NSS = SPI_NSS_SOFT;
-  hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
-  hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi2.Init.CRCPolynomial = 10;
-  if (HAL_SPI_Init(&hspi2) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+	/* SPI2 parameter configuration*/
+	hspi2.Instance = SPI2;
+	hspi2.Init.Mode = SPI_MODE_MASTER;
+	hspi2.Init.Direction = SPI_DIRECTION_2LINES;
+	hspi2.Init.DataSize = SPI_DATASIZE_8BIT;
+	hspi2.Init.CLKPolarity = SPI_POLARITY_LOW;
+	hspi2.Init.CLKPhase = SPI_PHASE_1EDGE;
+	hspi2.Init.NSS = SPI_NSS_SOFT;
+	hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+	hspi2.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	hspi2.Init.TIMode = SPI_TIMODE_DISABLE;
+	hspi2.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+	hspi2.Init.CRCPolynomial = 10;
+	if (HAL_SPI_Init(&hspi2) != HAL_OK)
+	{
+		_Error_Handler(__FILE__, __LINE__);
+	}
 
 }
 
 /** Configure pins as 
-        * Analog 
-        * Input 
-        * Output
-        * EVENT_OUT
-        * EXTI
-*/
+ * Analog
+ * Input
+ * Output
+ * EVENT_OUT
+ * EXTI
+ */
 static void MX_GPIO_Init(void)
 {
 
-  GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_InitTypeDef GPIO_InitStruct;
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, LD4_Pin|LD3_Pin, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOC, LD4_Pin|LD3_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LD4_Pin LD3_Pin */
-  GPIO_InitStruct.Pin = LD4_Pin|LD3_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+	/*Configure GPIO pins : LD4_Pin LD3_Pin */
+	GPIO_InitStruct.Pin = LD4_Pin|LD3_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 }
 
@@ -219,44 +282,44 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @param  file: The file name as string.
-  * @param  line: The line in file as a number.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @param  file: The file name as string.
+ * @param  line: The line in file as a number.
+ * @retval None
+ */
 void _Error_Handler(char *file, int line)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  while(1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	while(1)
+	{
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t* file, uint32_t line)
 { 
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+	/* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /**
-  * @}
-  */
+ * @}
+ */
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
